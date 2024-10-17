2a31487cafe6d3b57f4004476256fcb047f776de
Arturo Bautista <artugro.artr@icloud.com>
2024-10-17 17:04:26 -0600

    Update theme to dark mode

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./utils/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
-  darkTheme: "dark",
-  darkMode: ["selector", "[data-theme='dark']"],
+  darkTheme: "light",
+  darkMode: ["selector", "[data-theme='light']"],
   // DaisyUI theme colors
   daisyui: {
     themes: [
